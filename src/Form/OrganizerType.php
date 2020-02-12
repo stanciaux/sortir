6ad7075346406a3d6ebf019671227a62538398e2
OrvaultGrandval <guillaume.grandval2019@campus-eni.fr>
2020-02-12 17:25:44 +0100

    Form sortie middle+

--- src/Form/OrganizerType.php
+++ src/Form/OrganizerType.php
@@ -3,11 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Sortie;
-use App\Entity\Ville;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,17 +15,17 @@ class OrganizerType extends AbstractType
         $builder
             ->add('nom')
             ->add('dateDebut')
+            ->add('duree')
             ->add('dateCloture')
             ->add('nbInscriptionsMax')
             ->add('descriptionInfos')
-            ->add('duree')
-//            ->add('dateSortie')
-//            ->add('urlPhoto')
-//            ->add('site')
-//            ->add('etat')
+            ->add('urlPhoto')
+            ->add('dateSortie')
+            ->add('site')
+            ->add('etat')
             ->add('lieu')
-//            ->add('organisateur')
-//            ->add('participants')
+            ->add('organisateur')
+            ->add('participants')
             ->add('Valider', SubmitType::class)
         ;
     }
@@ -38,5 +35,7 @@ class OrganizerType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Sortie::class,
         ]);
+
+
     }
 }
