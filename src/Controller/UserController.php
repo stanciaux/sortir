29f478aea67191e9a2fdc8175492fa7ace71e946
madame-doussaye <cecile.dousset2019@campus-eni.fr>
2020-02-21 09:34:14 +0100

    commit modif pseudo

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,13 +23,24 @@ class UserController extends AbstractController
     public function update($id, EntityManagerInterface $em, Request $request, UserPasswordEncoderInterface $pwdEncoder)
     {
         $user = $em->getRepository(User::class)->find($id);
+        $users = $em->getRepository(User::class)->findAll();
         $userUpdateForm = $this->createForm(UserType::class, $user);
         $userUpdateForm->handleRequest($request);
         if ($userUpdateForm->isSubmitted() && $userUpdateForm->isValid()) {
 
+            $pseudo = $userUpdateForm->get('pseudo')->getData();
+            if ($pseudo){
+                foreach ($users as $utilisateur){
+                    $pseudoExist = $utilisateur->getPseudo();
+                    if ($pseudo == $pseudoExist){
+                        $this->addFlash('warning', "Ce pseudo est déjà pris, veuillez en choisir un autre");
+                        return $this->redirectToRoute('userupdate', ['id'=>$id]);
+                    }
+                }
+            }
+
             // récupération d'un nouveau mot de passe s'il y en a un de passé dans le champ "nouveau mdp"
             $password = $userUpdateForm->get('password')->getData();
-
             // S'il y a un nouveau mdp, on le hash et le charge en bdd
             if ($password) {
 //                dump($user->getPassword());
